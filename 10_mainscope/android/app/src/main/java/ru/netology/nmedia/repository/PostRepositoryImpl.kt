2b800106d3b40c0379e397ee86cf2e2349786d8b
Matveev <maopa@bk.ru>
2025-06-14 14:27:44 +0300

    Some changes

--- 10_mainscope/android/app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ 10_mainscope/android/app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -17,19 +17,7 @@ class PostRepositoryImpl(private val dao: PostDao) : PostRepository {
     override val data = dao.getAll().map(List<PostEntity>::toDto)
 
     override suspend fun getAll() {
-        try {
-            val response = PostsApi.service.getAll()
-            if (!response.isSuccessful) {
-                throw ApiError(response.code(), response.message())
-            }
 
-            val body = response.body() ?: throw ApiError(response.code(), response.message())
-            dao.insert(body.toEntity())
-        } catch (e: IOException) {
-            throw NetworkError
-        } catch (e: Exception) {
-            throw UnknownError
-        }
     }
 
     override suspend fun save(post: Post) {
@@ -49,23 +37,46 @@ class PostRepositoryImpl(private val dao: PostDao) : PostRepository {
     }
 
     override suspend fun removeById(id: Long) {
-        val response = PostsApi.service.removeById(id)
-        if (response.isSuccessful) {
-            dao.removeById(id)
-        } else {
-            throw ApiError(response.code(), response.message())
+        try {
+            val response = PostsApi.service.getAll()
+
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            dao.insert(body.toEntity())
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            throw UnknownError
         }
     }
 
     override suspend fun likeById(id: Long) {
-        val response = PostsApi.service.likeById(id)
-        if (response.isSuccessful) {
-            val post = response.body() ?: throw ApiError(-1, "No response body")
-            val postEntity = PostEntity.fromDto(post)
-            dao.update(postEntity)
-        } else {
-            throw ApiError(response.code(), response.message())
+        val post = dao.getById(id) ?: throw Resources.NotFoundException()
+        val backup = post.copy()
+        val isLiked = post.likedByMe
+        val newLikedState = !isLiked
+
+        val updatedPost = post.copy(
+            likedByMe = newLikedState,
+            likes = if (newLikedState) post.likes + 1 else post.likes - 1
+        )
+        dao.update(updatedPost)
+
+        try {
+            val response = if (newLikedState) {
+                PostsApi.service.likeById(id)
+            } else {
+                PostsApi.service.dislikeById(id)
+            }
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+        } catch (e: Exception) {
+            dao.update(backup)
+            throw e
         }
     }
-
 }
